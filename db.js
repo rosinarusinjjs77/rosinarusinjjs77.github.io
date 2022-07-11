0698c0d9cd43cebbb73cbe1dfe9375cd9f8aa3ca
rosinarusinjjs77 <rosinarusinjjs77@gmail.com>
2022-07-11 17:05:47 +0800

    1.0.0

---
+++ db.js
@@ -0,0 +1,27 @@
+let DB = { 
+  0: {
+    "tomidnight": 60,
+    "description": "The initial setting of the Doomsday Clock. Lihua, updated on 2022.7.11"
+  },
+  220224: {
+    "tomidnight": 59,
+    "description": "Russia Ukraine War broke out."
+  },
+  220328: {
+    "tomidnight": 58,
+    "description": "Shanghai was locked down."
+  },
+  220610: {
+    "tomidnight": 57.5,
+    "description": "A man beat up a girl violently because of sexual harassment in a barbecue shop in Tangshan, China."
+  },
+  220708: {
+    "tomidnight": 57,
+    "description": "Japan's ex-leader Shinzo Abe assassinated during a speech."
+  },
+  220711: {
+    "tomidnight": 56.5,
+    "description": "China crushes mass protest by bank depositors demanding their life savings back."
+  },
+
+}
---
+++ doomsday.js
@@ -0,0 +1,55 @@
+let YEARS = []
+
+$(document).ready(function() {
+    Object.keys(DB).forEach(function(key) {
+        YEARS.push(key)
+    })
+    
+    $("#prev").on("click", function (event) {
+        clock(getPrev($("#year").val()))
+    })
+    $("#next").on("click", function (event) {
+        clock(getNext($("#year").val()))
+    })
+    $(document).on("keyup", function (event) {
+        if (event.keyCode === 37) {
+            clock(getPrev($("#year").val()))
+        }
+        if (event.keyCode == 39) {
+            clock(getNext($("#year").val()))
+        }
+    })
+    clock(YEARS[0])
+})
+
+function clock(year) {
+    year = parseInt(year)
+    $("#year").val(year)
+    
+    updateText(year);
+    
+    let deg = DB[year].tomidnight * -6
+    $(".clock .minute").css('transform', 'rotateZ(calc('+deg+'deg)');
+}
+
+function updateText(year) {
+    $("#description").removeClass("animation")
+    var lastdesc = $("#description").attr("desc")
+    $("#description").attr("desc", DB[year].description)
+    $("#description").attr("lastdesc", lastdesc)
+    $("#description").focus(); //forces relayout to restart animation
+    $("#description").addClass("animation");
+}
+
+function getNext(year){
+    let index = YEARS.indexOf(year)
+    if (index == YEARS.length-1) return YEARS[0]
+    return YEARS[index+1]
+}
+
+function getPrev(year) {
+    let index = YEARS.indexOf(year)
+    if (index == 0) return YEARS[YEARS.length -1]
+    return YEARS[index-1]
+}
+
